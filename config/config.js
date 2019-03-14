556378e48b5ec9b68753b8e0ec2c57ebede9605c
Marcelo Araya <mjbacr97@gmail.com>
2019-03-13 22:35:08 -0600

    Establecida la conexion con mLab

--- config/config.js
+++ config/config.js
@@ -9,12 +9,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 //====================================
 //Configuración database
 //====================================
-let urlDB  = 'mongodb://localhost:27017/android-node';
+let urlDB;
 if(process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/android-node';
-    
 }else{
-    urlDB = 'mongodb://chelito:admin1@ds127015.mlab.com:27015/android-node';
+    urlDB = 'mongodb://chelito:123456a@ds213896.mlab.com:13896/android-node';
 }
 
 process.env.uriDB = urlDB;
