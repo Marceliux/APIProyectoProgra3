e7d80c12a10eabc0feb30fef2cc298379d197da8
Marcelo Araya <mjbacr97@gmail.com>
2019-03-15 00:54:06 -0600

    Probando Basde de datos mongodbAtlas

--- config/config.js
+++ config/config.js
@@ -13,7 +13,11 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/android-node';
 }else{
-    urlDB = 'mongodb://chelito:123456a@ds213896.mlab.com:13896/android-node';
+    urlDB = 'mongodb+srv://chelito:kXyJIQ3xBzHO34GT@cluster0-rrpjo.mongodb.net/android-node';
+
+    //Mlab
+    //'mongodb://chelito:123456a@ds213896.mlab.com:13896/android-node';
+            
 }
 
 process.env.uriDB = urlDB;
